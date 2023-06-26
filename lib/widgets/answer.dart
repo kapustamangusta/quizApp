466db4be584e96c8745a2685d010f082b3501fdf
Artem Sochinskiy <sochinskiyartom@gmail.com>
2023-06-26 10:40:59 +0300

    Добавил новые свойства классу Answer и обернул в его методе build виджет Container в виджет GestureDetector

--- lib/widgets/answer.dart
+++ lib/widgets/answer.dart
@@ -2,39 +2,44 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final String title;
+  final isCorrect;
+  final Function onChangeAnswer;
 
-  const Answer({super.key, required this.title});
+  const Answer({super.key, required this.title, this.isCorrect, required this.onChangeAnswer});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 50,
-        vertical: 5,
-      ),
-      padding: const EdgeInsets.all(10),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            blurRadius: 10,
-            offset: Offset(1, 1),
+    return GestureDetector(
+      onTap: () => onChangeAnswer(isCorrect),
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 50,
+          vertical: 5,
+        ),
+        padding: const EdgeInsets.all(10),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 10,
+              offset: Offset(1, 1),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(30),
+          gradient: LinearGradient(colors: [
+            Color(0xFF5537ff),
+            Color(0xFF8131ff),
+            Color(0xFFbd27ff),
+          ]),
+        ),
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.white,
           ),
-        ],
-        borderRadius: BorderRadius.circular(30),
-        gradient: LinearGradient(colors: [
-          Color(0xFF5537ff),
-          Color(0xFF8131ff),
-          Color(0xFFbd27ff),
-        ]),
-      ),
-      child: Text(
-        title,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 16,
-          color: Colors.white,
         ),
       ),
     );
